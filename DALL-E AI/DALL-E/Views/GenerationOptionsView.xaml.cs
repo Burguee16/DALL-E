aec2547bfaba863ea95665ffb8ef79378b31de42
burgueee <burguee16@gmail.com>
2024-08-20 22:13:56 -0300

    Martes 20 agosto 2024

--- DALL-E AI/DALL-E/Views/GenerationOptionsView.xaml.cs
+++ DALL-E AI/DALL-E/Views/GenerationOptionsView.xaml.cs
@@ -1,9 +1,15 @@
+using DALL_E.Models;
+
 namespace DALL_E.Views;
 
 public partial class GenerationOptionsView : ContentPage
 {
 	public List<string> Options { get; set; }
-	public GenerationOptionsView()
+
+    public List<ArtStyle> Styles { get; set; }
+
+
+    public GenerationOptionsView()
 	{
 		InitializeComponent();
 		FillOptions();
@@ -17,6 +23,12 @@ public partial class GenerationOptionsView : ContentPage
 			"Word",
 			"Winter",
 			"Tress"
+		};
+        Styles = new List<ArtStyle>()
+		{
+			new ArtStyle() { Name = "Dot Net", ImageUrl = "dotnet_bot.png" },
+			new ArtStyle() { Name = "Henry", ImageUrl = "dotnet_bot.png" },
+			new ArtStyle() { Name = "Mady", ImageUrl = "dotnet_bot.png" }
 		};
 	}
 }
