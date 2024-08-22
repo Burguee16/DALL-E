a0620da11e4b6551eab6ae497d1ab0fed11deaec
burgueee <burguee16@gmail.com>
2024-08-22 07:22:07 -0300

    Jueves 22 de Agosto del 2024 matutino

--- DALL-E AI/DALL-E/Views/ImageGeneratorView.xaml.cs
+++ DALL-E AI/DALL-E/Views/ImageGeneratorView.xaml.cs
@@ -11,10 +11,26 @@ public partial class ImageGeneratorView : ContentPage
     {
 		StopGeneration();
     }
-	private void StopGeneration()
+	private async void StopGeneration()
 	{
+		await Task.Delay(2000);
+
 		lottie.IsAnimationEnabled = false;
 		lottie.IsVisible = false;
 		ImageBorder.IsVisible = true;
+		ImageAnimation.IsVisible = true;
+
+		await Task.WhenAny(
+			ImageAnimation.ScaleTo(1.1, 1000),
+			ImageAnimation.FadeTo(0, 1000),
+            borderTime.ScaleTo(1, 1000),
+            borderTime.FadeTo(0, 1000)
+
+            );
+
+		await borderTime.FadeTo(0, 300);
+		await btnFinish.ScaleTo(1, 1000);
+
+
 	}
 }
